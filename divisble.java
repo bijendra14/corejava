7251004ee95ccc28837f8de35bbc21dbcb908ca6
bijendra14 <pal.bijendra91@gmail.com>
2021-06-24 12:54:17 +0530

    add

--- divisble.java
+++ divisble.java
@@ -1,9 +1,68 @@
-public class divisble {
-public static void main(String[] args){
-int n=50;
-if (n%7==0)
-System.out.println("divisible");
-else
-System.out.println("not divisible");
+<html>
+<head>  
+   <title>home page </title>
+   <style type="text/css">
+    /*.box{
+     border: 1px solid blue;height: 100px;width: 100px;float: left; background: grey
+    }*/
+.wrapper{
+    overflow:auto;
 }
+.box1{
+  background-color: white;
+  width: 35%;
+  float:left;
 }
+.box2{
+   
+    float:right;
+    width: 65%;
+  }
+.image-box
+{
+    height: 291px;
+    width:200px;
+    background-color: #e1eaff;
+    float: left;
+    border: 1px solid gray;
+    margin-left: 10px;
+     box-shadow: 0 1px 2px rgba(0, 0, 0, 0.15);
+  transition: box-shadow 0.3s ease-in-out;
+  }
+</style>
+</head>
+<body>
+  <div>
+    <a href="./home.html">home</a> 
+  	<a href="./election.html">election</a>
+    <a href="entertianment.html">entertiament</a>
+    <a href="firstweb.html">firstweb</a>
+  </div>
+  <div class="wrapper">
+  <div class="box1">
+    <h4>बड़ी ख़बरें</h4> 
+    <p>  अनिल देशमुख के खिलाफ FIR, संजय राउत ने साधा CBI पर निशाना </p>
+    <hr>
+    <p>    रूचिका जांगिड़ के इस गाने के लोग आज भी दीवाने, 430 मिलियन व्यूज </p>
+    <hr>
+    <p>    इस राज्‍य में स्‍थगित किए गए कक्षा 3 से 9 और 11वीं के फाइनल एग्‍जाम  </p>
+    <hr>
+    <p>    COVID रिसोर्सेज तेजी से खोजने के लिए Twitter लाया ये फीचर  </p>
+    <hr>
+  </div>
+  <div class="box2">
+      <div class="image-box"><a href="https://www.aajtak.in/coronavirus/story/ up-corona-news-10-arrested-for-black-marketing-of-remdesivir-in-lucknow-1243875-2021-04-24">
+      <img src="image/d4-sixteen_nine.webp">
+        <p><b>लखनऊ: 15 हजार में बेच रहे थे रेमडेसिविर, कोड से कर रहे थे कालाबाजारी, 10 गिरफ्तार</b> </p></a>
+      </div>
+      <div  class="image-box"><a href="https://www.aajtak.in/education/career/story/kerala-postal-circle-gds-recruitment-2021-india-post-bharti-gramin-dak-sevak-vacancy-sarkari-naukri-govt-job-alert-lbse-1243869-2021-04-24">
+      <img src="image/post_office1-sixteen_nine.jpg">
+      <p><b>डाक विभाग में 1421 पदों पर भर्ती, बिना परीक्षा मिलेगी नौकरी, आज आवेदन का आखिरी मौका </b> </p></a>
+      </div>
+      <div class="image-box"><a href="https://www.aajtak.in/coronavirus/story/us-study-covid-deaths-in-india-could-peak-by-mid-may-at-over-5000-per-day-1243855-2021-04-24"><img src="image/corona_2_4-sixteen_nine.webp">
+      <p><b>15 मई तक पीक पर होगा कोरोना संक्रमण, रोजाना होंगी 5600 मौतें, US स्टडी में दावा</b> </p></a>
+      </div>
+  </div>
+  </div>
+</body>
+</html>
---
+++ p1/src/p1/Forloop.java
@@ -0,0 +1,10 @@
+package p1;
+
+public class Forloop {
+	public static void main(String[] args) {
+		for(int shot=0; shot<=5; shot++) {
+			System.out.println(""+shot);
+		}
+	}
+
+}
